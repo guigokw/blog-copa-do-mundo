0aed1e5900593c42bd66ab79388726c65522a820
Luiz Guilherme Carvalho Ribeiro <guigo17ribeiro@gmail.com>
2026-05-21 10:43:16 -0300

    adicionando botão de contato e começando a estruturar o conteúdo 3

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sidebar = document.getElementById('sidebar');
 const openBtn = document.getElementById('open-btn');
 const closeBtn = document.getElementById('close-btn');
 const botoesVoltar = document.querySelectorAll('.volr-ini');
+const botaoContato = document.querySelectorAll('.bot-con');
 
         openBtn.addEventListener('click', () => {
             sidebar.classList.add('open');
@@ -14,6 +15,12 @@ const botoesVoltar = document.querySelectorAll('.volr-ini');
         botoesVoltar.forEach((botao) => {
             botao.addEventListener('click', () => {
                 window.scrollTo({top: 0, behavior: 'smooth'});
+            });
+        });
+
+        botaoContato.forEach((botao) => {
+            botao.addEventListener('click', () => {
+                window.location.href = 'contato.html';
             });
         });
 
